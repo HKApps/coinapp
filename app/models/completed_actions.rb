d0e3b538a84957c9d71d8325531fe49f10c495f3
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-12-14 09:16:49 -0800

    added validation and relationships

--- app/models/completed_actions.rb
+++ app/models/completed_actions.rb
@@ -1,2 +1,5 @@
 class CompletedActions < ActiveRecord::Base
+  belongs_to :schedule
+
+  validates_numericality_of :price, greater_than: 0
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :schedules
+
+  validates_presence_of :phone_number
+  validates :valid_phone_number
+
+  # TODO add phone_number validation
+  def valid_phone_number
+  end
 end
