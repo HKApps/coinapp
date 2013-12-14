8d076bb6791e9ccfd17e533db578b90331d62019
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-12-13 20:46:42 -0500

    created schedule model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214013807) do
+ActiveRecord::Schema.define(version: 20131214014009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schedules", force: true do |t|
+    t.integer  "user_id",                                           null: false
+    t.string   "type",                                              null: false
+    t.decimal  "price",      precision: 8, scale: 2
+    t.string   "comparison",                                        null: false
+    t.boolean  "enabled",                            default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "phone_number", null: false
