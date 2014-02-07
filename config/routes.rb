2f36b3bf4b3ea6d0d8f9b84ffbd070b8426dd94b
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-02-06 17:42:54 -0800

    add auto rendering of partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Coinapp::Application.routes.draw do
+    get 'partials/*partial' => 'partials#partial'
+
+    get '/*path', to: 'dashboards#index_template'
+
+    root to: 'dashboards#index_template'
 end
