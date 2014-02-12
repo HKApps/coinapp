d743aa860d0843f9fe55f05354b5a71404d8b20f
Matt Nguyen <mnguyen916@gmail.com>
2014-02-11 23:34:11 -0800

    Move config vars to env specific files

--- config/application.rb
+++ config/application.rb
@@ -19,14 +19,5 @@ module Coinapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Coinbase
-    config.coinbase_api_key    = ENV['COINBASE_API_KEY'] || '06df06a77ffa379be9dbe63edca7a70d46dcd73cddf2cdda2d9a06c8778d2a6d'
-    config.coinbase_api_secret = ENV['COINBASE_API_SECRET'] || 'f0012b96f172af7c42da890efcfa8b98d8dc32dbe31b9cac9b4c76f5f551864a'
-
-    # Twilio
-    config.twilio_sid        = ENV['TWILIO_ACCOUNT_SID'] || 'ACdbacbf5f4d0c5341f6ef363a4509f05a'
-    config.twilio_auth_token = ENV['TWILIO_AUTH_TOKEN'] || '7512f2ef0284748fbbb6f61261777cad'
-    config.twilio_number     = ENV['TWILIO_NUMBER'] || '+19163419340'
   end
 end
