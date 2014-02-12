4394ee8792f82d138abfec91b870cca45f604aca
Matt Nguyen <mnguyen916@gmail.com>
2014-02-11 16:29:17 -0800

    Remove schedule type
    'type' is a reserved column name

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,8 +2,9 @@ class Schedule < ActiveRecord::Base
   belongs_to :user
   has_many :completed_actions
 
-  validates_presence_of :type, :price, :user_id, :comparison, :enabled
+  validates_presence_of :price, :user_id, :comparison, :enabled
   validates_numericality_of :price, greater_than: 0
-  validates_inclusion_of :type, in: ["notification", "buy_order", "sell_order"]
   validates_inclusion_of :comparison, in: [">", "<"]
+
+  default_scope { where(enabled: true) }
 end
