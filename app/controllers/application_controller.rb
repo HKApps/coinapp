5ec943d3a6ad5371b276dfc93ae36ea48e8eedd9
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-02-14 13:24:47 -0500

    fixed current_user_session logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
+    @current_user_sesion || UserSession.find
   end
 
   def current_user
