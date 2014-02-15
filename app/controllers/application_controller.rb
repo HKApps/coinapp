efc9d0df9ffcc8d9dc36c25f8a0b151ba37e0016
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-02-15 15:19:03 -0500

    memoize current_user_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user_session
-    @current_user_sesion || UserSession.find
+    @current_user_sesion ||= UserSession.find
   end
 
   def current_user
