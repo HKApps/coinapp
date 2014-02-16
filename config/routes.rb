e18ba76f69267731a379a306d7ce1975cd4a83a9
Basil Siddiqui <basil.siddiqui@gmail.com>
2014-02-15 23:35:50 -0500

    added endpoints for schedules

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Coinapp::Application.routes.draw do
     match 'login', to: 'user_sessions#create', :via => [:get, :post]
     match 'logout', to: 'user_sessions#destroy', :via => [:get, :post]
 
-    resources :user_sessions
-    resources :users
+    resources :user_sessions, only: [:create, :destroy]
+    resources :users, only: [:show, :create]
+    resources :schedules, only: [:index, :create, :destroy] do
+      member do
+        post 'disable', to: 'schedules#disable'
+        post 'enable', to: 'schedules#enable'
+      end
+    end
 
     get 'partials/*partial' => 'partials#partial'
 
