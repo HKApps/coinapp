6c40c672c4cbacce90c987500fcc4a5e77a316dd
Matt Nguyen <mnguyen916@gmail.com>
2014-03-11 14:08:08 +0700

    Add /sidekiq endpoint for monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
+require 'sidekiq/web'
+
 Coinapp::Application.routes.draw do
-    match 'login', to: 'user_sessions#create', :via => [:get, :post]
-    match 'logout', to: 'user_sessions#destroy', :via => [:get, :post]
+  mount Sidekiq::Web, at: "/sidekiq"
 
-    resources :user_sessions, only: [:create, :destroy]
-    resources :users, only: [:show, :create]
-    resources :schedules, only: [:index, :create, :destroy] do
-      member do
-        post 'disable', to: 'schedules#disable'
-        post 'enable', to: 'schedules#enable'
-      end
+  root to: 'home#index_template'
+  get 'partials/*partial' => 'partials#partial'
+  get '/*path', to: 'home#index_template'
+
+  match 'login', to: 'user_sessions#create', :via => [:get, :post]
+  match 'logout', to: 'user_sessions#destroy', :via => [:get, :post]
+
+  resources :user_sessions, only: [:create, :destroy]
+  resources :users, only: [:show, :create]
+  resources :schedules, only: [:index, :create, :destroy] do
+    member do
+      post 'disable', to: 'schedules#disable'
+      post 'enable', to: 'schedules#enable'
     end
+  end
 
-    get 'partials/*partial' => 'partials#partial'
-
-    get '/*path', to: 'home#index_template'
-
-    root to: 'home#index_template'
 end
